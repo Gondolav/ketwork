b379fc8040a9bc90f97036ee80dc73e433c0af88
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-07-18 15:12:44 +0200

    Update dependencies

--- app/src/main/java/ch/gondolav/ketwork/MainActivity.kt
+++ app/src/main/java/ch/gondolav/ketwork/MainActivity.kt
@@ -1,29 +1,33 @@
 package ch.gondolav.ketwork
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        runOnConnection {
-//            off {
-//
-//            }
-//
-//            on {
-//
-//            }
-//        }
-//
-//        runIfConnected {
-//
-//        }
-//
-//        runIfDisconnected {
-//
-//        }
+        val textView = findViewById<TextView>(R.id.textView)
+
+        runOnConnection {
+            off {
+                textView.text = "Offline"
+            }
+
+            on {
+                textView.text = "Online"
+            }
+        }
+
+        runIfConnected {
+            textView.text = "Online"
+        }
+
+        runIfDisconnected {
+            textView.text = "Offline"
+        }
     }
 }
