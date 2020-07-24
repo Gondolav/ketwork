b23f0c186bea0b14044c1ce74e9cac6149fc4743
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-07-24 09:21:10 +0200

    Fix Gradle release bug related to obfuscation

--- app/src/main/java/ch/gondolav/ketwork/MainActivity.kt
+++ app/src/main/java/ch/gondolav/ketwork/MainActivity.kt
@@ -1,16 +1,13 @@
 package ch.gondolav.ketwork
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.textView)
 
         runOnConnection {
             off {
